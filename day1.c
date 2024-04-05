8ae394fcc127c790e264aa13418acff32ff1e274
Gourav7777 <360inside360@gmail.com>
2024-04-05 11:23:57 +0530

    1st day

--- day1.c
+++ day1.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
  main()
-{
-   printf("heelo world");
+{  
+
+ // Question 1
+   printf("Question 1 => \n");
+
+   printf("My name is Gourav\n");
+   printf("I am 24 Years Old\n");
+   printf("My school name is RED & WHITE\n");
+
+
+//  Question 2
+   printf("Question 2 => \n");
+
+   printf("--------\n");
+   printf("|      |\n");
+   printf("R      |\n");
+   printf("N      |\n");
+   printf("W      |\n");
+   printf("|      |\n");
+   printf("--------\n");
+
+//  Question 3  
+   printf("Question 3 => \n");
+
+   printf("*\n");
+   printf("**\n");
+   printf("***\n");
+   printf("**\n");
+   printf("*\n");
+   
+ //  Question 4  
+   printf("Question 4 => \n");
+
+   printf("*\n");
+   printf("*\n");
+   printf("*\n");
+   printf("*    * *        *\n");
+   printf("*   *   *      *\n");
+   printf("*  *     *    *\n");
+   printf("* *       *  *\n");
+   printf("*\n"); 
+
 }
